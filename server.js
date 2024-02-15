15e0a751655aae634b322d5a670d39aa1f8882bd
Nielven Mascariñas <mascarinas022@gmail.com>
2024-02-15 12:05:41 +0800

    add comments

--- server.js
+++ server.js
@@ -1,22 +1,25 @@
+// Import necessary modules
 const express = require("express");
 const app = express();
 const Joi = require("joi");
 
+// Import recipe data
 const { recipesData } = require("./src/data.js");
 
+// Middleware to parse JSON bodies
 app.use(express.json());
 
-//Get Request
+// Root route
 app.get("/", (req, res) => {
   res.send("Welcome to the root route of the API.");
 });
 
-//Shows all the data
+// Route to get all recipes
 app.get("/api/recipes", (req, res) => {
   res.send(recipesData);
 });
 
-//get a certain recipe by adding a ID argument
+// Route to get a specific recipe by ID
 app.get("/api/recipes/:id", (req, res) => {
   const schema = Joi.object({
     id: Joi.number().integer().required(),
@@ -25,17 +28,20 @@ app.get("/api/recipes/:id", (req, res) => {
   const { error } = schema.validate(req.params);
   if (error) return res.status(400).send(error.details[0].message);
 
+  // Find recipe by ID
   const ID = req.params.id;
   const recipe = recipesData.find((c) => c.id === parseInt(ID));
   if (!recipe) return res.status(404).send({ message: "Recipe not found." });
   res.send(recipe);
 });
 
-// get recipes by name
+// Route to get recipes by name
 app.get("/api/recipes/name/:name", (req, res) => {
+  // Create regex from name
   const name = req.params.name.replace(/\s/g, "");
   const regex = new RegExp(name.split("").join("\\s*"), "i");
 
+  // Find recipes by name
   const recipes = recipesData.filter((c) => regex.test(c.name));
 
   if (recipes.length === 0)
@@ -45,11 +51,13 @@ app.get("/api/recipes/name/:name", (req, res) => {
   res.send(recipes);
 });
 
-//get recipes by tag
+// Route to get recipes by tag
 app.get("/api/recipes/tag/:tag", (req, res) => {
+  // Create regex from tag
   const tag = req.params.tag.replace(/\s/g, "");
   const regex = new RegExp("^" + tag.split("").join("\\s*") + "$", "i");
 
+  // Find recipes by tag
   const recipes = recipesData.filter((c) => c.tag.some((t) => regex.test(t)));
 
   if (recipes.length === 0)
@@ -59,8 +67,9 @@ app.get("/api/recipes/tag/:tag", (req, res) => {
   res.send(recipes);
 });
 
-//Post Request
+// Route to create a new recipe
 app.post("/api/recipes", (req, res) => {
+  // Validate recipe data
   const schema = Joi.object({
     name: Joi.string().required(),
     ingredients: Joi.array().items(Joi.string()).required(),
@@ -71,6 +80,7 @@ app.post("/api/recipes", (req, res) => {
   const { error } = schema.validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
+  // Create new recipe
   const newRecipe = {
     id: recipesData.length + 1,
     name: req.body.name,
@@ -83,12 +93,14 @@ app.post("/api/recipes", (req, res) => {
   res.send(newRecipe);
 });
 
-//PUT request
+// Route to update a recipe
 app.put("/api/recipes/:id", (req, res) => {
+  // Find recipe by ID
   const ID = parseInt(req.params.id);
   const recipe = recipesData.find((c) => c.id === ID);
   if (!recipe) return res.status(404).send({ message: "Recipe not found." });
 
+  // Validate recipe data
   const schema = Joi.object({
     id: Joi.number().integer(),
     name: Joi.string(),
@@ -106,17 +118,20 @@ app.put("/api/recipes/:id", (req, res) => {
   res.send(recipe);
 });
 
-//delete request
+// Route to delete a recipe
 app.delete("/api/recipes/:id", (req, res) => {
+  // Find recipe by ID
   const ID = parseInt(req.params.id);
   const recipe = recipesData.find((c) => c.id === ID);
   if (!recipe) return res.status(404).send({ message: "Recipe not found." });
 
+  // Remove recipe from array
   const index = recipesData.indexOf(recipe);
   recipesData.splice(index, 1);
 
   res.send(recipe);
 });
 
+// Start the server
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Listening on http://localhost:${port}...`));
