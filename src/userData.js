b5edd04306a9b48f63af3f4a3b0a507aa2d351bf
Nielven Mascariñas <mascarinas022@gmail.com>
2024-02-16 12:36:50 +0800

    Add userData.js file with initial user data

---
+++ src/userData.js
@@ -0,0 +1,28 @@
+const users = [
+  {
+    id: 1,
+    name: "Nielven Mascarinas",
+    email: "nielven@example.com",
+    password: "password1",
+  },
+  {
+    id: 2,
+    name: "Cindy Rosales",
+    email: "cindy@example.com",
+    password: "password2",
+  },
+  {
+    id: 3,
+    name: "Aliza Rabe",
+    email: "maylyn@example.com",
+    password: "password3",
+  },
+  {
+    id: 4,
+    name: "Ryan Natividad",
+    email: "ryan@example.com",
+    password: "password4",
+  },
+];
+
+module.exports = { users };
